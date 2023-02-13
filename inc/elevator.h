1883e86f89b66cd1ca4d7f9298a9ae787116e0b8
ChristofferLeo <chrisroelofsen@gmail.com>
2023-02-13 17:30:07 +0100

    Starting work on queue

--- inc/elevator.h
+++ inc/elevator.h
@@ -1,6 +1,7 @@
 #ifndef INC_ELEVATOR_H
 #define INC_ELEVATOR_H
 #include "../inc/order.h"
+#include "../drivers/elevio.h"
 
 /* Datatypes */
 typedef enum ElevatorState {
@@ -17,6 +18,9 @@ typedef struct Elevator{
     int floor;
     ElevatorState state;
     Order order_list[20];
+
+    //Potential order 
+    int queue[N_FLOORS][N_BUTTONS];
 } Elevator;
 
 void innit();
